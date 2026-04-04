2639095b02201ded9c4081526c754a93f8a7df65
victor-main <vg280350@gmail.com>
2026-04-04 17:15:27 -0300

    premake (nao testei)

--- Hazel/src/Hazel/Core.h
+++ Hazel/src/Hazel/Core.h
@@ -12,8 +12,6 @@
         #define HAZEL_API __declspec(dllimport)
     #endif
 #elifdef HZ_PLATFORM_LINUX
-    // No Linux (GCC/Clang), usamos atributos de visibilidade
-    // O HZ_BUILD_DLL diz: "Estou a compilar a biblioteca agora, por isso exporta"
     #ifdef HZ_BUILD_DLL
         #define HAZEL_API __attribute__((visibility("default")))
     #else

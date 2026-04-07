1bb820836086851b696702b5b2d9eef8868b729e
victor-main <vg280350@gmail.com>
2026-04-06 22:12:32 -0300

    glm math config

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -2,11 +2,6 @@
 // Created by victor on 4/4/26.
 //
 
-#include "Hazel/Events/Event.h"
-#include "Hazel/Events/KeyEvent.h"
-#include "Hazel/Input.h"
-#include "Hazel/KeyCodes.h"
-#include "Hazel/Log.h"
 #include <Hazel.h>
 
 class ExampleLayer : public Hazel::Layer
@@ -14,16 +9,17 @@ class ExampleLayer : public Hazel::Layer
 public:
     ExampleLayer() : Layer("Example") {}
 
-    void OnUpdate() override {
+    void OnUpdate() override
+    {
         if (Hazel::Input::IsKeyPressed(HAZEL_KEY_TAB))
-        HAZEL_TRACE("Tab key is pressed (poll)");
+            HAZEL_TRACE("Tab key is pressed (poll)");
     }
 
     void OnEvent(Hazel::Event &event) override
     {
         if (event.GetEventType() == Hazel::EventType::KeyPressed)
         {
-            auto &e = static_cast<Hazel::KeyPressedEvent &>(event);
+            const auto &e = dynamic_cast<Hazel::KeyPressedEvent &>(event);
             if (e.GetKeyCode() == HAZEL_KEY_TAB)
                 HAZEL_TRACE("Tab key is pressed (event)");
             HAZEL_TRACE("{0}", static_cast<char>(e.GetKeyCode()));
